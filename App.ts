7d4eed8413d322af345e0390110d7065e91545e5
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-11-15 05:28:49 +0500

    work complete

---
+++ App.ts
@@ -0,0 +1,11 @@
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+const button = document.querySelector("button")!;
+
+const add = (n1, n2) => {
+  return n1 + n2;
+};
+
+button.addEventListener("click", () => {
+  console.log(add(input1.value, input2.value));
+});

f7d1abedbdf1ffbe134563343be673a0ee78cbf6
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-11-17 03:52:25 +0500

    work complete

--- App.ts
+++ App.ts
@@ -1,11 +1,19 @@
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-const button = document.querySelector("button")!;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button")!;
 
-const add = (n1: number, n2 : number) => {
-  return n1 + n2;
-};
+// const add = (n1: number, n2 : number) => {
+//   return n1 + n2;
+// };
 
-button.addEventListener("click", () => {
-  console.log(add(+input1.value, +input2.value));
-});
+// button.addEventListener("click", () => {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+
+// primitive types
+
+// boolean true/false
+// string "Abc"
+// number 12, 5.5, -23
+// etc 

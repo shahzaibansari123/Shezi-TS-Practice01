f44391edbca1ad0a775ce49a9f0ffbf0ad98d0b0
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-11-16 20:04:20 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,9 @@
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+var button = document.querySelector("button");
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
